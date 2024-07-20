07c2471bb2c42e014e650225505bcab1b3867f1c
howetuft <howetuft@gmail.com>
2024-07-20 22:52:59 +0200

    Add PostBuildWheel

---
+++ PostBuildWheel.cmake
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2024 Howetuft
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# Stats
+MESSAGE(STATUS "Add stat command")
+add_custom_target(stats ALL)
+add_dependencies(stats pyluxcore)
+add_custom_command(TARGET stats POST_BUILD COMMAND sccache --show-stats)
+
+# Install
+install(TARGETS pyluxcore LIBRARY DESTINATION lib)
