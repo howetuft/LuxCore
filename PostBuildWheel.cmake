2ecc8d2a45caa4329979224ea4480030a47639a6
howetuft <howetuft@gmail.com>
2024-07-21 09:19:08 +0200

    Tweak PostBuildWheel

--- PostBuildWheel.cmake
+++ PostBuildWheel.cmake
@@ -4,7 +4,7 @@
 
 # Stats
 MESSAGE(STATUS "Add stat command")
-add_custom_target(stats ALL)
+add_custom_target(stats ALL COMMAND "echo 'Stats'")
 add_dependencies(stats pyluxcore)
 add_custom_command(TARGET stats POST_BUILD COMMAND sccache --show-stats)
 
