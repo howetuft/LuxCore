b85cbcf7416e3addec007f472987d8e92223d0cd
howetuft <howetuft@gmail.com>
2025-04-06 08:43:19 +0200

    Remove outdated/unmaintained build scripts

--- scripts/azurepipeline/macos/codesign.sh
+++
@@ -1,8 +0,0 @@
-_mount_dir="$1"
-_entitlements="./scripts/azurepipeline/macos/entitlements.plist"
-
-echo "Codesigning .dylib and .so libraries"
-for f in $(find "${_mount_dir}" -name "*.dylib" -o -name "*.so"); do
-   codesign --remove-signature "${f}"
-   codesign --timestamp --options runtime --entitlements="${_entitlements}" --sign "26B60C66D3DABEE289D6CD90948F6F036F19C3CD" "${f}"
-done
