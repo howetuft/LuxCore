1171975358567ed1b6612345f4f5cf962462a6ab
howetuft <howetuft@gmail.com>
2025-04-06 22:25:51 +0200

    Lint get_preset.py

--- build-helpers/make/get_preset.py
+++ build-helpers/make/get_preset.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+"""Helper to get CMake presets."""
+
 import subprocess
 import sys
 import re
@@ -11,37 +13,60 @@ if __name__ == "__main__":
 
     try:
         res = subprocess.run(
-            ["cmake", "--list-presets"],
+            [
+                "cmake",
+                "--list-presets",
+            ],
             capture_output=True,
             text=True,
             check=True,
         )
     except FileNotFoundError as err:
         # if environment variable is set...
-        if "CONAN_PRESET" in os.environ and (conan_preset:= os.environ["CONAN_PRESET"]):
+        if "CONAN_PRESET" in os.environ and (
+            conan_preset := os.environ["CONAN_PRESET"]
+        ):
             print(conan_preset)
-            exit(0)
+            sys.exit(0)
         else:
-            print(str(err), file=sys.stderr)
-            exit(1)
+            print(
+                str(err),
+                file=sys.stderr,
+            )
+            sys.exit(1)
     except subprocess.CalledProcessError as err:
-        print(err.stderr, file=sys.stderr)
-        exit(1)
+        print(
+            err.stderr,
+            file=sys.stderr,
+        )
+        sys.exit(1)
 
     lines = res.stdout.splitlines()
 
     try:
         line = lines[2]
     except IndexError:
-        print("ERROR: bad cmake output:", res.stdout, file=sys.stderr)
-        print(res.stderr, file=sys.stderr)
-        exit(1)
+        print(
+            "ERROR: bad cmake output:",
+            res.stdout,
+            file=sys.stderr,
+        )
+        print(
+            res.stderr,
+            file=sys.stderr,
+        )
+        sys.exit(1)
 
-    res = re.findall('"([^"]*)"', line)
+    res = re.findall(
+        '"([^"]*)"',
+        line,
+    )
 
     for preset in res:
         print(preset)
         break
     else:
-        print("ERROR: no preset found in cmake output", line)
-
+        print(
+            "ERROR: no preset found in cmake output",
+            line,
+        )
