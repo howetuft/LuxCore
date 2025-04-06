44cd82b0634d0cd988ce877a17fec60f872fc112
howetuft <howetuft@gmail.com>
2025-04-06 22:24:51 +0200

    Lint conanfile.py

--- build-helpers/conan/conanfile.py
+++ build-helpers/conan/conanfile.py
@@ -2,23 +2,28 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+"""Conan recipe for LuxCore."""
+
+
 from pathlib import Path
-import io
 import os
 import json
 
 from conan import ConanFile
-from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.cmake import CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.env import VirtualBuildEnv
 
 THIS_FILE = Path(__file__)
 BUILD_SETTINGS_FILE = THIS_FILE.parent / ".." / "build-settings.json"
 
-with open(BUILD_SETTINGS_FILE) as config:
+with open(BUILD_SETTINGS_FILE, encoding="utf-8") as config:
     config_json = json.load(config)
     LUXDEPS_VERSION = config_json["Dependencies"]["release"]
 
+
 class LuxCore(ConanFile):
+    """Conan recipe."""
+
     name = "luxcore"
     version = "2.10.0"
     user = "luxcore"
@@ -28,86 +33,99 @@ class LuxCore(ConanFile):
     tool_requires = "ninja/[*]"
     settings = "os", "compiler", "build_type", "arch"
 
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.absolute_paths = True
-        tc.variables["CMAKE_COMPILE_WARNING_AS_ERROR"] = False
+    def _generate_oidn(self, toolchain):
+        """Generate toolchain part related to oidn."""
+        self_settings_os = self.settings.os  # pylint: disable=no-member
 
         # OIDN denoiser executable
         oidn = self.dependencies["luxcoredeps"].dependencies["oidn"]
-        oidn_info = oidn.cpp_info
         oidn_version = oidn.ref.version
-        oidn_bindir = Path(oidn_info.bindirs[0])
-        if self.settings.os == "Windows":
+        oidn_bindir = Path(oidn.cpp_info.bindirs[0])
+        if self_settings_os == "Windows":
             denoise_path = oidn_bindir / "oidnDenoise.exe"
         else:
             denoise_path = oidn_bindir / "oidnDenoise"
-        tc.variables["LUX_OIDN_DENOISE_PATH"] = denoise_path.as_posix()
+        toolchain.variables["LUX_OIDN_DENOISE_PATH"] = denoise_path.as_posix()
 
         # OIDN denoiser cpu (for Linux)
-        oidn_libdir = Path(oidn_info.libdirs[0])
-        tc.variables["LUX_OIDN_DENOISE_LIBS"] = oidn_libdir.as_posix()
-        tc.variables["LUX_OIDN_DENOISE_BINS"] = oidn_bindir.as_posix()
-        tc.variables["LUX_OIDN_VERSION"] = oidn_version
-        if self.settings.os == "Linux":
+        oidn_libdir = Path(oidn.cpp_info.libdirs[0])
+        toolchain.variables["LUX_OIDN_DENOISE_LIBS"] = oidn_libdir.as_posix()
+        toolchain.variables["LUX_OIDN_DENOISE_BINS"] = oidn_bindir.as_posix()
+        toolchain.variables["LUX_OIDN_VERSION"] = oidn_version
+        if self_settings_os == "Linux":
             denoise_cpu = (
-                oidn_libdir / f"libOpenImageDenoise_device_cpu.so.{oidn_version}"
+                oidn_libdir
+                / f"libOpenImageDenoise_device_cpu.so.{oidn_version}"
             )
-        elif self.settings.os == "Windows":
+        elif self_settings_os == "Windows":
             denoise_cpu = oidn_bindir / "OpenImageDenoise_device_cpu.dll"
-        elif self.settings.os == "Macos":
+        elif self_settings_os == "Macos":
             denoise_cpu = (
-                oidn_libdir / f"OpenImageDenoise_device_cpu.{oidn_version}.pylib"
+                oidn_libdir
+                / f"OpenImageDenoise_device_cpu.{oidn_version}.pylib"
             )
-        tc.variables["LUX_OIDN_DENOISE_CPU"] = denoise_cpu.as_posix()
+        else:
+            raise RuntimeError(f"OIDN: Unhandled os ({self_settings_os})")
+        toolchain.variables["LUX_OIDN_DENOISE_CPU"] = denoise_cpu.as_posix()
 
-        # nvrtc
-        if self.settings.os in ("Linux", "Windows"):
-            nvrtc = self.dependencies["luxcoredeps"].dependencies["nvrtc"]
-            nvrtc_info = nvrtc.cpp_info
-            if self.settings.os == "Linux":
-                nvrtc_dir = Path(nvrtc_info.libdirs[0])
-            else:
-                nvrtc_dir = Path(nvrtc_info.bindirs[0])
-                tc.cache_variables["LUX_NVRTC_BINS"] = nvrtc_dir.as_posix()
+    def _generate_nvrtc(self, toolchain):
+        """Generate toolchain part related to nvrtc."""
+        if not (
+            self_settings_os := self.settings.os  # pylint: disable=no-member
+        ) in (
+            "Linux",
+            "Windows",
+        ):
+            return
 
-            nvrtc_libs = [
-                f.as_posix()
-                for f in nvrtc_dir.iterdir()
-                if f.is_file()
-            ]
-            tc.cache_variables["LUX_NVRTC"] = ";".join(nvrtc_libs)
+        nvrtc = self.dependencies["luxcoredeps"].dependencies["nvrtc"]
+        nvrtc_info = nvrtc.cpp_info
+        if self_settings_os == "Linux":
+            nvrtc_dir = Path(nvrtc_info.libdirs[0])
+        else:
+            nvrtc_dir = Path(nvrtc_info.bindirs[0])
+            toolchain.cache_variables["LUX_NVRTC_BINS"] = nvrtc_dir.as_posix()
 
+        nvrtc_libs = [f.as_posix() for f in nvrtc_dir.iterdir() if f.is_file()]
+        toolchain.cache_variables["LUX_NVRTC"] = ";".join(nvrtc_libs)
 
-        # CMAKE_OSX_ARCHITECTURES
-        if self.settings.os == "Macos" and self.settings.arch == "armv8":
-            tc.cache_variables["CMAKE_OSX_ARCHITECTURES"] = "arm64"
-
-
-        # Bison/Flex
+    def _generate_bison_flex(self, toolchain):
+        """Generate toolchain part related to bison and flex."""
         # FLEX_EXECUTABLE and BISON_EXECUTABLE are recognized by
         # standard CMake FindFlex and FindBison
-        if self.settings.os == "Windows":
+        if self.settings.os == "Windows":  # pylint: disable=no-member
             # winflexbison
-            winflexbison = self.dependencies["luxcoredeps"].dependencies["winflexbison"]
+            winflexbison = self.dependencies["luxcoredeps"].dependencies[
+                "winflexbison"
+            ]
 
-            lex_path = os.path.join(winflexbison.package_folder, "bin", "win_flex.exe").replace("\\", "/")
-            tc.cache_variables["FLEX_EXECUTABLE"] = lex_path
+            lex_path = os.path.join(
+                winflexbison.package_folder, "bin", "win_flex.exe"
+            ).replace("\\", "/")
+            toolchain.cache_variables["FLEX_EXECUTABLE"] = lex_path
 
-            yacc_path = os.path.join(winflexbison.package_folder, "bin", "win_bison.exe").replace("\\", "/")
-            tc.cache_variables["BISON_EXECUTABLE"] = yacc_path
+            yacc_path = os.path.join(
+                winflexbison.package_folder, "bin", "win_bison.exe"
+            ).replace("\\", "/")
+            toolchain.cache_variables["BISON_EXECUTABLE"] = yacc_path
         else:
             # *nix Flex and Bison
             # CMake variables for CMake to find Bison & Flex (relying on
             # standard FindBison and FindFlex
             flex = self.dependencies["luxcoredeps"].dependencies["flex"]
-            flex_path = os.path.join(flex.package_folder, "bin", "flex").replace("\\", "/")
-            tc.cache_variables["FLEX_EXECUTABLE"] = flex_path
+            flex_path = os.path.join(
+                flex.package_folder, "bin", "flex"
+            ).replace("\\", "/")
+            toolchain.cache_variables["FLEX_EXECUTABLE"] = flex_path
 
             bison = self.dependencies["luxcoredeps"].dependencies["bison"]
-            bison_path = os.path.join(bison.package_folder, "bin", "bison").replace("\\", "/")
-            tc.cache_variables["BISON_EXECUTABLE"] = bison_path
-            bison.cpp_info.set_property("cmake_find_mode", "none")  # Force use of standard CMake FindBISON
+            bison_path = os.path.join(
+                bison.package_folder, "bin", "bison"
+            ).replace("\\", "/")
+            toolchain.cache_variables["BISON_EXECUTABLE"] = bison_path
+            bison.cpp_info.set_property(
+                "cmake_find_mode", "none"
+            )  # Force use of standard CMake FindBISON
 
             # Environment variables for Bison/Flex/m4 to work together
             buildenv = VirtualBuildEnv(self)
@@ -119,29 +137,54 @@ class LuxCore(ConanFile):
             buildenv.environment().define_path("BISON_PKGDATADIR", pkgdir)
 
             # https://github.com/conda-forge/bison-feedstock/issues/7
-            m4 = self.dependencies["luxcoredeps"].dependencies["m4"]
-            m4_path = os.path.join(m4.package_folder, "bin", "m4").replace("\\", "/")
-            tc.cache_variables["CONAN_M4_PATH"] = m4_path
+            m4_path = os.path.join(
+                self.dependencies["luxcoredeps"]
+                .dependencies["m4"]
+                .package_folder,
+                "bin",
+                "m4",
+            ).replace("\\", "/")
+            toolchain.cache_variables["CONAN_M4_PATH"] = m4_path
             buildenv.environment().define_path("M4", m4_path)
 
-
             buildenv.generate()
-            tc.presets_build_environment = buildenv.environment()
+            toolchain.presets_build_environment = buildenv.environment()
 
-        tc.cache_variables["SPDLOG_FMT_EXTERNAL_HO"] = True
-        tc.cache_variables["LUXCOREDEPS_VERSION"] = str(self.dependencies["luxcoredeps"].ref.version)
+    def generate(self):
+        """Generate toolchain and dependencies."""
+        toolchain = CMakeToolchain(self)
+        toolchain.absolute_paths = True
 
-        tc.generate()
+        # OIDN
+        self._generate_oidn(toolchain)
 
-        cd = CMakeDeps(self)
+        # Nvidia runtime compiler (nvrtoolchain)
+        self._generate_nvrtoolchain(toolchain)
 
-        cd.generate()
+        # Bison/Flex
+        self._generate_bison_flex(toolchain)
+
+        # Variables
+        self_settings_os = self.settings.os  # pylint: disable=no-member
+        self_settings_arch = self.settings.arch  # pylint: disable=no-member
+
+        toolchain.variables["CMAKE_COMPILE_WARNING_AS_ERROR"] = False
+        if self_settings_os == "Macos" and self_settings_arch == "armv8":
+            toolchain.cache_variables["CMAKE_OSX_ARCHITECTURES"] = "arm64"
+        toolchain.cache_variables["SPDLOG_FMT_EXTERNAL_HO"] = True
+        toolchain.cache_variables["LUXCOREDEPS_VERSION"] = str(
+            self.dependencies["luxcoredeps"].ref.version
+        )
+
+        toolchain.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def layout(self):
+        """Define package layout."""
         # Mandatory to get a CMakeUserPresets.json, see
         # https://docs.conan.io/2/reference/tools/cmake/cmaketoolchain.html
         cmake_layout(
-            self,
-            src_folder=Path("..", ".."),
-            generator="Ninja Multi-Config"
+            self, src_folder=Path("..", ".."), generator="Ninja Multi-Config"
         )
