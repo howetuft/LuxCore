ea19f34fca08ab1a5c7436ea6242c93bb96b5a44
howetuft <howetuft@gmail.com>
2025-04-10 07:03:59 +0200

    Recompose windows wheels: adapt to 'wheel/0.46.1' version

--- build-helpers/wheels/recompose_wheel_windows.py
+++ build-helpers/wheels/recompose_wheel_windows.py
@@ -2,17 +2,61 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import os
+"""This script makes a final rearrangement to a Windows wheel.
+
+2 actions to be done:
+- Rename and move oidnDenoise.exe
+- Rename and move OpenImageDenoise_device_cpu.dll
+"""
+
 from pathlib import Path
 import argparse
 import tempfile
 import shutil
+import subprocess
+import sys
 
 from wheel.wheelfile import WheelFile
-from wheel.cli.unpack import unpack
-from wheel.cli.pack import pack
+
+
+def unpack(path, dest):
+    """Unpack a wheel."""
+    args = [
+        sys.executable,
+        "-m",
+        "wheel",
+        "unpack",
+        f"--dest={dest}",
+        str(path),
+    ]
+    try:
+        output = subprocess.check_output(args, text=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as err:
+        print(err.output)
+        raise
+    print(output)
+
+
+def pack(directory, dest_dir):
+    """(Re)pack a wheel."""
+    args = [
+        sys.executable,
+        "-m",
+        "wheel",
+        "pack",
+        f"--dest-dir={dest_dir}",
+        str(directory),
+    ]
+    try:
+        output = subprocess.check_output(args, text=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as err:
+        print(err.output)
+        raise
+    print(output)
+
 
 def main():
+    """Main entry point."""
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("wheelpath", type=Path)
@@ -26,8 +70,8 @@ def main():
     with tempfile.TemporaryDirectory() as tmpdir:  # Working space
         # Unpack wheel
         unpack(path=args.wheelpath, dest=tmpdir)
-        with WheelFile(args.wheelpath) as wf:
-            namever = wf.parsed_filename.group("namever")
+        with WheelFile(args.wheelpath) as wheelfile:
+            namever = wheelfile.parsed_filename.group("namever")
             unpacked_wheel_path = Path(tmpdir) / namever
 
         # Rename and move oidnDenoise
@@ -38,17 +82,23 @@ def main():
         )
 
         # Rename and move OpenImageDenoise_device_cpu
-        print("Rename OpenImageDenoise_device_cpu.pyd into OpenImageDenoise_device_cpu.dll")
+        print(
+            "Rename OpenImageDenoise_device_cpu.pyd "
+            "into OpenImageDenoise_device_cpu.dll"
+        )
         shutil.move(
-            unpacked_wheel_path / "pyluxcore.libs" / "OpenImageDenoise_device_cpu.pyd",
-            unpacked_wheel_path / "pyluxcore.libs" / "OpenImageDenoise_device_cpu.dll",
+            unpacked_wheel_path
+            / "pyluxcore.libs"
+            / "OpenImageDenoise_device_cpu.pyd",
+            unpacked_wheel_path
+            / "pyluxcore.libs"
+            / "OpenImageDenoise_device_cpu.dll",
         )
 
         # Repack wheel
         pack(
             directory=unpacked_wheel_path,
             dest_dir=wheel_folder,
-            build_number=None
         )
 
 
