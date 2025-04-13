d38f40f5e016a5c53a7ddb03fa996f94952dfbd1
howetuft <howetuft@gmail.com>
2025-04-13 17:09:08 +0200

    Documentation: all-in-one

--- build-helpers/cmake/doxylux.cmake
+++ build-helpers/cmake/doxylux.cmake
@@ -2,52 +2,68 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+# https://stackoverflow.com/questions/8247189/doxygen-is-slow/8247993#8247993
+# Submodules are just tagged, and main target merges everything
 
+
+set(DOC_SOURCE_DIR ${PROJECT_SOURCE_DIR}/doxygen)
+set(DOC_BUILD_DIR ${PROJECT_BINARY_DIR}/doc)
 
 if (NOT TARGET doc)
-  add_custom_target(doc)
+  if(DOXYGEN_FOUND)
+    configure_file("${DOC_SOURCE_DIR}/all.template.in" "${DOC_BUILD_DIR}/Doxyfile")
+    doxygen_add_docs(doc
+      ${DOC_SOURCE_DIR}/main.md
+      WORKING_DIRECTORY ${DOC_BUILD_DIR}
+      COMMENT "Generating API documentation"
+      CONFIG_FILE ${DOC_BUILD_DIR}/Doxyfile
+    )
+  else()
+    message(AUTHOR_WARNING "Doxygen not found, could not create documentation")
+  endif(DOXYGEN_FOUND)
 endif()
 
 macro(generate_doc target)
   if(DOXYGEN_FOUND)
 
-    # Generate doxygen.template
-    set(DOXYGEN_${target}_TEMPLATE_IN ${PROJECT_SOURCE_DIR}/doxygen/${target}.template.in)
-    set(DOXYGEN_${target}_TEMPLATE ${PROJECT_BINARY_DIR}/doxygen/${target}.template)
-    configure_file("${DOXYGEN_${target}_TEMPLATE_IN}" "${DOXYGEN_${target}_TEMPLATE}")
-
     # Set Doxygen input and output
-    set(DOXYGEN_${target}_INPUT ${PROJECT_BINARY_DIR}/doc/doxygen-${target}.conf)
-    set(DOXYGEN_${target}_OUTPUT_DIR ${PROJECT_BINARY_DIR}/doc/${target})
-    set(DOXYGEN_${target}_OUTPUT ${DOXYGEN_${target}_OUTPUT_DIR}/html/index.html)
+    set(DOXYGEN_INPUT_CONFIG ${DOC_BUILD_DIR}/doxygen-${target}.conf)
+    set(DOXYGEN_OUTPUT_TAGFILE ${DOC_BUILD_DIR}/${target}.tag)
+    set(DOXYGEN_OUTPUT_HTML ${DOC_BUILD_DIR}/html/${target})
 
-    message(STATUS "Doxygen ${target} output: " ${DOXYGEN_${target}_OUTPUT})
+    message(STATUS "Doxygen ${target} tags output: " ${DOXYGEN_OUTPUT_TAGFILE})
+    message(STATUS "Doxygen ${target} html output: " ${DOXYGEN_OUTPUT_HTML})
+
+    # Generate doxygen.template
+    set(DOXYGEN_TEMPLATE_IN ${DOC_SOURCE_DIR}/${target}.template.in)
+    configure_file("${DOXYGEN_TEMPLATE_IN}" "${DOXYGEN_INPUT_CONFIG}")
+
 
     if(DOXYGEN_DOT_FOUND)
         message(STATUS "Found dot")
         set(DOXYGEN_DOT_CONF "HAVE_DOT = YES")
     endif(DOXYGEN_DOT_FOUND)
 
+    # Generate tag file
     add_custom_command(
-      OUTPUT ${DOXYGEN_${target}_OUTPUT}
-      # Creating custom doxygen-xxx.conf
-      COMMAND mkdir -p ${DOXYGEN_${target}_OUTPUT_DIR}
-      COMMAND cp ${DOXYGEN_${target}_TEMPLATE} ${DOXYGEN_${target}_INPUT}
-      COMMAND echo "INPUT = " ${PROJECT_SOURCE_DIR}/include/${target}  ${PROJECT_SOURCE_DIR}/src/${target} >> ${DOXYGEN_${target}_INPUT}
-      COMMAND echo "OUTPUT_DIRECTORY = " ${DOXYGEN_${target}_OUTPUT_DIR} >> ${DOXYGEN_${target}_INPUT}
-      COMMAND echo ${DOXYGEN_DOT_CONF} >> ${DOXYGEN_${target}_INPUT}
+      OUTPUT ${DOXYGEN_OUTPUT_TAGFILE} ${DOXYGEN_OUTPUT_HTML}
+      # Customizing doxygen-xxx.conf
+      COMMAND echo "INPUT = " ${PROJECT_SOURCE_DIR}/include/${target}  ${PROJECT_SOURCE_DIR}/src/${target} >> ${DOXYGEN_INPUT_CONFIG}
+      COMMAND echo "OUTPUT_DIRECTORY = " ${DOC_BUILD_DIR} >> ${DOXYGEN_INPUT_CONFIG}
+      COMMAND echo ${DOXYGEN_DOT_CONF} >> ${DOXYGEN_INPUT_CONFIG}
       # Launch doxygen
-      COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_${target}_INPUT}
-      DEPENDS ${DOXYGEN_${target}_TEMPLATE}
+      COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_INPUT_CONFIG}
+      DEPENDS ${DOXYGEN_INPUT_CONFIG}
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     )
 
+    # Generate html
 
-    add_custom_target(doc-${target} DEPENDS ${DOXYGEN_${target}_OUTPUT})
+    add_custom_target(doc-${target}
+      DEPENDS ${DOXYGEN_OUTPUT_TAGFILE} ${DOXYGEN_OUTPUT_HTML}
+    )
     add_dependencies(doc doc-${target})
     message(STATUS "Target 'doc-${target}' declared for ${target} documentation")
-  else()
-    message(AUTHOR_WARNING "Doxygen not found, could not create ${target} documentation")
   endif(DOXYGEN_FOUND)
 
 endmacro()
