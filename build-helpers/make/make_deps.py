228afc47b354f929990fb5076cc0d96a83434ea7
howetuft <howetuft@gmail.com>
2025-04-29 04:06:37 +0200

    MacOS Intel: set min version to 11.0

--- build-helpers/make/make_deps.py
+++ build-helpers/make/make_deps.py
@@ -468,6 +468,8 @@ def main(
             "--settings=build_type=Release",
             f"--conf:all=tools.cmake.cmaketoolchain:generator={generator}",
         ]
+        if platform.system() == "Darwin" and platform.machine() == "x86_64":
+            main_block += ["--settings=os.version=11.0"]
         build_types = [
             "Debug",
             "Release",
