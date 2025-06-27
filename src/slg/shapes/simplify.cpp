c842acaf5742a88f1131717aa462cb98f8b21835
howetuft <howetuft@gmail.com>
2025-06-27 06:21:13 +0200

    Simplify - parallel invocation of Flipped

--- src/slg/shapes/simplify.cpp
+++ src/slg/shapes/simplify.cpp
@@ -29,6 +29,7 @@
 
 #include <tbb/parallel_for.h>
 #include <tbb/concurrent_vector.h>
+#include <tbb/parallel_invoke.h>
 
 
 #include "luxrays/core/exttrianglemesh.h"
@@ -474,8 +475,13 @@ private:
 		// Don't remove if flipped
 		// deleted0, deleted1: true/false if the triangles referencing the
 		// vertex are deleted
-		auto [will_flip0, deleted0] = Flipped(p, i0, i1);
-		auto [will_flip1, deleted1] = Flipped(p, i1, i0);
+		std::invoke_result_t<decltype(&Simplify::Flipped), Simplify, Point, u_int, u_int> res0, res1;
+		tbb::parallel_invoke(
+			[&]{res0 = Flipped(p, i0, i1);},
+			[&]{res1 = Flipped(p, i1, i0);}
+		);
+		auto [will_flip0, deleted0] = res0;
+		auto [will_flip1, deleted1] = res1;
 		if (will_flip0 || will_flip1) {
 			return deletedTriangles;
 		}
