a450f32c41eb6d68ba86d9b0eef6f3efa39bd286
howetuft <howetuft@gmail.com>
2025-12-27 00:10:49 +0100

    Make.bat: quick & dirty mode for debugging (Windows)

--- make.bat
+++ make.bat
@@ -8,6 +8,8 @@ REM SPDX-License-Identifier: Apache-2.0
 REM Convenience wrapper for CMake commands
 
 REM Script command (1st parameter)
+
+call setup_x64.bat
 set COMMAND=%1
 
 
@@ -61,6 +63,8 @@ if "%COMMAND%" == "" (
     call :WheelTest
 ) else if "%COMMAND%" == "msvc-init" (
     call :MsvcInit
+) else if "%COMMAND%" == "windebug" (
+    call :WinDebug
 ) else (
     echo Unknown command: "%COMMAND%"
 )
@@ -113,6 +117,20 @@ set CCACHE_DIRECT=true
 set CCACHE_DEPEND=true
 call setup_x64.bat
 goto :EOF
+
+:WinDebug
+REM Special (quick & dirty) custom config for debugging on Windows platform
+REM Run it under cmd.exe
+REM 'setup_x64.bat' must be in PATH
+call :MsvcInit
+set LUX_BUILD_TYPE=Debug
+set LUX_SANITIZER=1
+set CMAKE_BUILD_PARALLEL_LEVEL=1
+call :Config
+call :BuildAndInstall luxcore
+call :BuildAndInstall luxcoreui
+call :BuildAndInstall luxcoreconsole
+call :BuildAndInstall pyluxcore 
 
 :Clear
 REM rmdir /S /Q
